48483e69b53226f1fc91cefd3e6a4b44f751ce08
SaneenKP <saneekp2000@gmail.com>
2024-01-04 22:39:47 +0530

    new example for none

--- none.kt
+++ none.kt
@@ -4,15 +4,26 @@
 
 fun main(){
 
+    val allFalse = listOf(false , false , false , false , false)
+
+    //since no value is true ie, no element matches the predicate.
+    val isAllFalse = allFalse.none{
+        it == true 
+    }
+
+    println("is all false -- $isAllFalse")
+
     val listOfNonPrimeNumbers = listOf(4,6,8,10,12,14) 
     val listOfNonPrimeNumbersWithAPrimeNumber = listOf(2,4,5,6,8,10,12,14)
 
+    //will return true since not even a single element confirms true for the predicate.
     var isNoneOfTheNumberAPrime = listOfNonPrimeNumbers.none{
         isNumberAPrime(it)
     }
 
     println("None of The Numbers are prime for a list of Non Prime Numbers = $isNoneOfTheNumberAPrime")
 
+    //will return false as 1 element will confirm true for the predicate.
     isNoneOfTheNumberAPrime = listOfNonPrimeNumbersWithAPrimeNumber.none{
         isNumberAPrime(it)
     }
